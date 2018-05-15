9085c712b7bc4bd79d6d0d3b4079fa850bd2f99c
bgregg <bgregg@grubhub.com>
2018-05-15 00:30:07 -0500

    Better code snippets

--- src/reveal/slides.cljs
+++ src/reveal/slides.cljs
@@ -63,6 +63,35 @@
                 " #{:a \"mixed\" :set}"]]
       [:code.hljs.clojure code])]])
 
+(defn highlight [& args]
+  [:pre {:style "width: 50%; float:left;"}
+   (for [code args]
+     [:code.hljs.clojure code "\n"])])
+
+(def slide-6
+  [:section
+   [:h2 "ClojureScript Forms"]
+   (highlight "(def var-name \"value\");global"
+              (s/join "\n"
+                ["(let [var-name 42 ;locals "
+                 "      other [\"foo\" \"bar\"]]"
+                 "  ;vars accessible here"
+                 ") "])
+              (s/join "\n"
+                ["(fn [arg1 arg2 & more] "
+                 "  ;function body)"
+                 "#(some-fn %1 %2) ;short syntax"]))
+   (highlight "(my-fn arg1 arg2) ;call my-fn "
+              (s/join "\n"
+                ["(if (test?) "
+                 "  ;then"
+                 "  ;else"
+                 ") "])
+              (s/join "\n"
+                ["(loop [var-name []]"
+                 "  (recur"
+                 "    (my-fn var-name 42)))"]))])
+
 (defn all
   "Add here all slides you want to see in your presentation."
   []
@@ -70,4 +99,5 @@
    slide-2
    slide-3
    slide-4
-   slide-5])
+   slide-5
+   slide-6])
